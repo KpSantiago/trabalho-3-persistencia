d369d52e44025d9cacc83be7f35a32453b69cdf7
narciso-roberto <narcisoroberto1369@gmail.com>
2026-01-05 14:59:23 -0300

    backUp

--- src/models/transacao.py
+++ src/models/transacao.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
 from datetime import datetime;
+from models.produto import Produto
 
 class Transacao(BaseModel):
     quantidade: int 
     data_transacao: datetime | None
-    listaDosProdutos: list[str] # lista de string, pois essa lista ira armazenas os ID's dos produtos
+    listaDosProdutos: list[Produto] 
 
 
 
